40f8a404f87b0961658d7f904e5b30d7c6396daf
Jeff Davey <jeffdavey@submersion.com>
2026-04-08 20:15:04 -0600

    Update project to .NET 8 for Vintage Story 1.21.x
    Convert csproj from old-style .NET Framework 4.8 to SDK-style .NET 8,
    update DLL paths to Linux install, and bump version to 1.1.0.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,43 +1,8 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 using Vintagestory.API.Common;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("resregrow2")]
-[assembly: AssemblyDescription("")]
-//[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
-
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3c74c2c4-dd60-4ff2-a28b-99f179513774")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
-
-[assembly: ModInfo( "resregrow2", "resregrow2",
-    Version = "1.0.0",
+[assembly: ModInfo("resregrow2", "resregrow2",
+    Version = "1.1.0",
     Description = "Remake of Resin Regrow",
     Authors = new[] { "Nietsnie" })]
 
- [assembly: ModDependency("game")]
+[assembly: ModDependency("game")]
